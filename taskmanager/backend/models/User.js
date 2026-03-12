4c22b84f97f23cdea4fe963ac2209c27172194ea
Saim Naqi <91535315+saim-n-dev@users.noreply.github.com>
2026-03-12 21:11:05 +0500

    Add files via upload

---
+++ taskmanager/backend/models/User.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+    {
+        name: {type: String, required: true},
+        email: {type: String, required: true, unique:true},
+        password: {type: String, required: true},
+        profileImageURL: {type: String, default: null},
+        role: {type: String, enum: ["admin", "member"], default: "member"}, // role-based access
+    },
+      {  timestamps: true }
+);
+
+module.exports =mongoose.model("User", UserSchema);
